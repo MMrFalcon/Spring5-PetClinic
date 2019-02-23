7ba2b1747f38a9115f2c98f7b8b092dd775bba10
MMrFalcon <kubasokol123456@gmail.com>
2019-02-23 15:51:50 +0100

    Implement Interfaces for services. Closes #5

---
+++ PetClinicData/src/main/java/falcon/springframework/spring5petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package falcon.springframework.spring5petclinic.services;
+
+import falcon.springframework.spring5petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet owner);
+
+    Set<Vet> findAll();
+}
