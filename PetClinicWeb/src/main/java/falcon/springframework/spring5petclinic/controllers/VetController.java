0367257738e230dd505180ed197dd8cda174b8f9
MMrFalcon <kubasokol123456@gmail.com>
2019-02-25 22:37:18 +0100

    Added Vets List, Controller update. Closes #12

--- PetClinicWeb/src/main/java/falcon/springframework/spring5petclinic/controllers/VetController.java
+++ PetClinicWeb/src/main/java/falcon/springframework/spring5petclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package falcon.springframework.spring5petclinic.controllers;
 
+import falcon.springframework.spring5petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    String vetsList(){
+    String vetsList(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
