9b609584990c40f948a7c35d1a9c3efc1b85b77a
MMrFalcon <kubasokol123456@gmail.com>
2019-03-09 15:56:45 +0100

    Pet Types Bootstrap Initialization. Closes #28

--- PetClinicWeb/src/main/java/falcon/springframework/spring5petclinic/bootstrap/DataInitializer.java
+++ PetClinicWeb/src/main/java/falcon/springframework/spring5petclinic/bootstrap/DataInitializer.java
@@ -1,8 +1,10 @@
 package falcon.springframework.spring5petclinic.bootstrap;
 
 import falcon.springframework.spring5petclinic.model.Owner;
+import falcon.springframework.spring5petclinic.model.PetType;
 import falcon.springframework.spring5petclinic.model.Vet;
 import falcon.springframework.spring5petclinic.services.OwnerService;
+import falcon.springframework.spring5petclinic.services.PetTypeService;
 import falcon.springframework.spring5petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,10 +14,12 @@ public class DataInitializer implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
@@ -24,6 +28,16 @@ public class DataInitializer implements CommandLineRunner {
         /**
          * Creation of simple POJO when application starts
          */
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        petTypeService.save(cat);
+
+        System.out.println("Loaded Pet Types");
 
         Owner falcon = new Owner();
         falcon.setFirstName("Jacob");
