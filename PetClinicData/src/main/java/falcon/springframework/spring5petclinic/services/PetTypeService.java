e73feaa6355e1741054b6ab82f82fcd506122436
MMrFalcon <kubasokol123456@gmail.com>
2019-03-09 15:44:00 +0100

    Created PetType Map Service Impl. Closes #25

---
+++ PetClinicData/src/main/java/falcon/springframework/spring5petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package falcon.springframework.spring5petclinic.services;
+
+import falcon.springframework.spring5petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
