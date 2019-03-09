287c9f6ccc9aa34518251afada3f19fbe5d458df
MMrFalcon <kubasokol123456@gmail.com>
2019-03-09 15:09:23 +0100

    Adding contact info. Closes #24

--- PetClinicData/src/main/java/falcon/springframework/spring5petclinic/model/Owner.java
+++ PetClinicData/src/main/java/falcon/springframework/spring5petclinic/model/Owner.java
@@ -4,6 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String phone;
+    private String city;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     private Set<Pet> pets;
 
     public Set<Pet> getPets() {
