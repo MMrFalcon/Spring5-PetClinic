468b0a12bbcc5433c3bbf31f55a3835c63a4842c
MMrFalcon <kubasokol123456@gmail.com>
2019-03-16 18:34:04 +0100

    Creation of Speciality Service and Mapper class. Closes #27

---
+++ PetClinicData/src/main/java/falcon/springframework/spring5petclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package falcon.springframework.spring5petclinic.services;
+
+import falcon.springframework.spring5petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
