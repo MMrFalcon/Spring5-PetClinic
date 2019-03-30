6f999883df68f665ffa98bc5e6ea4a501f7da46c
MMrFalcon <kubasokol123456@gmail.com>
2019-03-30 14:07:02 +0100

    Convert Visits to JPA Entity. Closes #34

--- PetClinicData/src/main/java/falcon/springframework/spring5petclinic/model/Visit.java
+++ PetClinicData/src/main/java/falcon/springframework/spring5petclinic/model/Visit.java
@@ -1,11 +1,20 @@
 package falcon.springframework.spring5petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
     private LocalDate date;
     private String description;
+
+    @ManyToMany
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
