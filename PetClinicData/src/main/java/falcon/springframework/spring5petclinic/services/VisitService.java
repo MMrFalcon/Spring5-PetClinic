c082544246d53a4bbb779e3542f755b08d644e17
MMrFalcon <kubasokol123456@gmail.com>
2019-03-30 15:25:57 +0100

    Creation of Visit Map Service. Closes #42

---
+++ PetClinicData/src/main/java/falcon/springframework/spring5petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package falcon.springframework.spring5petclinic.services;
+
+import falcon.springframework.spring5petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ PetClinicData/src/main/java/falcon/springframework/spring5petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,41 @@
+package falcon.springframework.spring5petclinic.services.map;
+
+import falcon.springframework.spring5petclinic.model.Visit;
+import falcon.springframework.spring5petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null ||
+                                                                    visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }else {
+            return super.save(visit);
+        }
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
