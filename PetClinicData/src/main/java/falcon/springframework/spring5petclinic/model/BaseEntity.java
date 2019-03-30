79541fc9e19321256fbd583cbb356cd3bfc61942
MMrFalcon <kubasokol123456@gmail.com>
2019-03-30 12:23:46 +0100

    Implement Base Entity. Closes #31

--- PetClinicData/src/main/java/falcon/springframework/spring5petclinic/model/BaseEntity.java
+++ PetClinicData/src/main/java/falcon/springframework/spring5petclinic/model/BaseEntity.java
@@ -1,12 +1,17 @@
 package falcon.springframework.spring5petclinic.model;
 
-public class BaseEntity {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
 
     public void setId(Long id) {
         this.id = id;
