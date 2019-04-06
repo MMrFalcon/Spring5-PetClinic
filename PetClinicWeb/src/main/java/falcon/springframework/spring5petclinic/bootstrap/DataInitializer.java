1df3ef356703379a881879407d3401b5036be57f
MMrFalcon <kubasokol123456@gmail.com>
2019-04-06 18:44:59 +0200

    Bootstrapping Visits on Startup. Closes #45

--- PetClinicWeb/src/main/java/falcon/springframework/spring5petclinic/bootstrap/DataInitializer.java
+++ PetClinicWeb/src/main/java/falcon/springframework/spring5petclinic/bootstrap/DataInitializer.java
@@ -1,10 +1,7 @@
 package falcon.springframework.spring5petclinic.bootstrap;
 
 import falcon.springframework.spring5petclinic.model.*;
-import falcon.springframework.spring5petclinic.services.OwnerService;
-import falcon.springframework.spring5petclinic.services.PetTypeService;
-import falcon.springframework.spring5petclinic.services.SpecialityService;
-import falcon.springframework.spring5petclinic.services.VetService;
+import falcon.springframework.spring5petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,17 @@ public class DataInitializer implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
+    private final PetService petService;
 
-    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+                           SpecialityService specialityService, VisitService visitService, PetService petService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
+        this.petService = petService;
     }
 
     @Override
@@ -73,9 +75,16 @@ public class DataInitializer implements CommandLineRunner {
         jacobsPet.setBirthDate(LocalDate.of(2017,3,22));
         jacobsPet.setOwner(falcon);
         jacobsPet.setName("Pimpek");
-        falcon.getPets().add(jacobsPet);
+        falcon.getPets().add(petService.save(jacobsPet));
 
         ownerService.save(falcon);
+
+        Visit dogVisit = new Visit();
+        dogVisit.setPet(jacobsPet);
+        dogVisit.setDate(LocalDate.now());
+        dogVisit.setDescription("That was visit");
+
+        visitService.save(dogVisit);
 
         Owner geralt = new Owner();
         geralt.setFirstName("Geralt");
@@ -89,12 +98,12 @@ public class DataInitializer implements CommandLineRunner {
         geraltsPet.setBirthDate(LocalDate.of(2008,11,13));
         geraltsPet.setName("Plotka");
         geraltsPet.setOwner(geralt);
-        geralt.getPets().add(geraltsPet);
+        geralt.getPets().add(petService.save(geraltsPet));
 
 
         ownerService.save(geralt);
 
-        System.out.println("Loaded owners and their pets");
+        System.out.println("Loaded owners and their pets with visits");
 
         Vet sam = new Vet();
         sam.setFirstName("Sam");
