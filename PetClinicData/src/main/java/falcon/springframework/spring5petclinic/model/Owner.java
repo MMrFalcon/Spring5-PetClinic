635bdf813b85b354fda5c41ad68a983723253916
MMrFalcon <kubasokol123456@gmail.com>
2019-04-25 23:50:22 +0200

    Code refactoring. Adding builder pattern to the Person. #48

--- PetClinicData/src/main/java/falcon/springframework/spring5petclinic/model/Owner.java
+++ PetClinicData/src/main/java/falcon/springframework/spring5petclinic/model/Owner.java
@@ -1,6 +1,9 @@
 package falcon.springframework.spring5petclinic.model;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -13,10 +16,20 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Table(name = "owners")
 public class Owner extends Person {
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city,
+                 String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.phone = telephone;
+
+        if(pets != null) {
+            this.pets = pets;
+        }
+    }
 
     private String address;
     private String phone;
