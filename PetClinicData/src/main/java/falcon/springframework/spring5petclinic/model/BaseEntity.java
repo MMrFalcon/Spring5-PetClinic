0c960be086e8bf774464103d8c45e0a9dee63a8f
MMrFalcon <kubasokol123456@gmail.com>
2019-07-06 19:48:21 +0200

    Adding/Updating Owner. Closes #52

--- PetClinicData/src/main/java/falcon/springframework/spring5petclinic/model/BaseEntity.java
+++ PetClinicData/src/main/java/falcon/springframework/spring5petclinic/model/BaseEntity.java
@@ -19,4 +19,9 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    public boolean isNew() {
+        return this.id == null;
+    }
 }
