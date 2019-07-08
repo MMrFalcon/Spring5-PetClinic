297aa3510708c01cd83a92686e9221ff18d79436
MMrFalcon <kubasokol123456@gmail.com>
2019-07-09 00:15:11 +0200

    Adding LocalDate formatter + PetTypeFormatter component. #53

---
+++ PetClinicWeb/src/main/java/falcon/springframework/spring5petclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,38 @@
+package falcon.springframework.spring5petclinic.formatters;
+
+import falcon.springframework.spring5petclinic.model.PetType;
+import falcon.springframework.spring5petclinic.services.PetTypeService;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> foundPetTypes = petTypeService.findAll();
+
+        for (PetType type : foundPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+}
